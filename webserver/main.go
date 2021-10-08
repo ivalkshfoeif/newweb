bd0fbf5ff1209d79052ba61d1ed5c9914eb4e962
ivalkshfoeif <61750044+ivalkshfoeif@users.noreply.github.com>
2021-10-08 01:54:05 -0400

    test

--- webserver/main.go
+++ webserver/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func firstPage(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<h1> Hello, this is my first page! </h>")
+	io.WriteString(w, "<h1> Hello, World! </h>")
 }
 
 func main() {
